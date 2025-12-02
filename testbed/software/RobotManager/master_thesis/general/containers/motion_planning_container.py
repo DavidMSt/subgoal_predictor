70e8506f60c772f12dd13529b97e558244f4f386
David Stoll <david.michael.stoll@gmail.com>
2025-12-02 15:17:55 +0100

    added overarching motion planning container similar to agent and envrionment ones

--- testbed/software/RobotManager/master_thesis/general/containers/motion_planning_container.py
+++ testbed/software/RobotManager/master_thesis/general/containers/motion_planning_container.py
@@ -1,5 +1,27 @@
 from dataclasses import dataclass
 import numpy as np
+from core.utils.states import State # TODO: do I even need this statee import here?
+
+@dataclass(frozen = False, slots=True)
+class MotionPlanningState:
+    success: bool = False
+    message: str | None = None
+
+    # raw planner output
+    raw_states: list[np.ndarray] | None = None
+    raw_inputs: list[np.ndarray] | None = None
+    raw_durations: list[float] | None = None
+    raw_delta_t: float | None = None
+
+    # optional post-processing
+    smoothed_states: list[np.ndarray] | None = None
+    smoothed_inputs: list[np.ndarray] | None = None
+    smoothed_delta_t: float | None = None
+
+    # timing / metrics
+    computation_time: float | None = None
+    path_length: float | None = None
+    cost: float | None = None
 
 @dataclass(frozen = True, slots = True)
 class MotionPlanningConfig:
@@ -23,3 +45,8 @@ class MotionPlanningConfig:
     # control bounds (only relevant for kinodynamic case)
     theta_dot_bounds: tuple = (-np.pi/3, np.pi/3)
     v_bounds: tuple = (-1.0, 1.0)
+
+@dataclass(slots = True)
+class MotionPlannigContainer:
+    state: MotionPlanningState
+    config: MotionPlanningConfig
