25fe45a8ffb68864a13caf6c4dc4ecd49266b500
David Stoll <david.michael.stoll@gmail.com>
2025-12-26 18:51:50 +0100

    minor cosmetics

--- testbed/software/RobotManager/master_thesis/modules/motion_planning/mp_simulation_module.py
+++ testbed/software/RobotManager/master_thesis/modules/motion_planning/mp_simulation_module.py
@@ -2,10 +2,7 @@
 from core.utils.logging_utils import Logger
 
 # master thesis
-from master_thesis.modules.motion_planning.mp_agent_module import MPAgentModule
 from master_thesis.containers.module_containers.mp_containers.mp_planner_container import AgentMPPlannerContainer
-from master_thesis.general.general_agent import FRODOGeneralAgent
-from master_thesis.containers.general_containers.task_container import TaskContainer
 
 class MPSimulationModule():
     agent_mp_conts : dict[str, AgentMPPlannerContainer]
@@ -19,7 +16,6 @@ class MPSimulationModule():
 
     def start_motion_planning(self, phase_name = 'example_mp_phase'):
         agent_mp_conts = self.agent_mp_conts
-        if not agent_mp_conts:
-            self.logger.error('MP Module coudld not find MP agent containers')
+
         for _, mp_cont in agent_mp_conts.items():
             mp_cont.start_planning = phase_name
